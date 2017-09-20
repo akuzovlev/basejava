a09f68f74e51220b31098cf878c9c3b40f110250
Alexander Kuzovlev <basistadom@gmail.com>
2017-09-20 21:55:58 +0300

    Первая попытка сделать дз

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,62 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int lastElement = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
+        lastElement = 0;
     }
 
     void save(Resume r) {
+        storage[lastElement] = r;
+        lastElement += 1;
     }
 
     Resume get(String uuid) {
+        for (Resume res : storage) {
+            if (res.toString().equals(uuid)) {
+                return res;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < 10000; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                if (lastElement == 1) {
+                    storage[i] = null;
+                    lastElement = 0;
+                    return;
+                } else {
+                    storage[i] = storage[lastElement - 1];
+                    storage[lastElement - 1] = null;
+                    lastElement -= 1;
+                    return;
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        Resume[] rez = new Resume[lastElement];
+        for (int i=0; i < lastElement; i++) {
+            rez[i] = storage[i];
+        }
+        return rez;
     }
 
     int size() {
-        return 0;
+        return lastElement;
     }
 }
