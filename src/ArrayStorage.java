390da22821e8673a03ca57fbb02fca361fe321d5
Alexander Kuzovlev <basistadom@gmail.com>
2017-09-21 10:36:02 +0300

    NPE error fixed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,6 +8,9 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
     int lastElement = 0;
 
+    Resume empty = new Resume();
+
+
     void clear() {
         Arrays.fill(storage, null);
         lastElement = 0;
@@ -15,31 +18,25 @@ public class ArrayStorage {
 
     void save(Resume r) {
         storage[lastElement] = r;
-        lastElement ++;
+        lastElement++;
     }
 
     Resume get(String uuid) {
-        for (Resume res : storage) {
-            if (res.toString().equals(uuid)) {
-                return res;
+        for (int i = 0; i < lastElement; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
             }
         }
-        return null;
+        return new Resume();
     }
 
     void delete(String uuid) {
         for (int i = 0; i < lastElement; i++) {
             if (storage[i].toString().equals(uuid)) {
-                if (lastElement == 1) {
-                    storage[i] = null;
-                    lastElement = 0;
-                    return;
-                } else {
-                    storage[i] = storage[lastElement - 1];
-                    storage[lastElement - 1] = null;
-                    lastElement --;
-                    return;
-                }
+                storage[i] = storage[lastElement - 1];
+                storage[lastElement - 1] = null;
+                lastElement--;
+                return;
             }
         }
     }
@@ -48,7 +45,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage,lastElement);
+        return Arrays.copyOf(storage, lastElement);
     }
 
     int size() {
