687ac850038c78e1426e3fc5a031713d71d385ad
Alexander Kuzovlev <basistadom@gmail.com>
2017-09-23 23:13:23 +0300

    HW2 first try

--- src/webapp/storage/ArrayStorage.java
+++ src/webapp/storage/ArrayStorage.java
@@ -17,35 +17,57 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        //TODO реализовать
-        //TODO проверка что существует  System.out.println("Resume ...") (бул существование в отдельный метод)
+        // проверка что существует  System.out.println("Resume ...")
+        int position = isExists(r.getUuid());
+        if ( position < 0) {
+            System.out.println("Resume is not exists in storage!");
+        } else {
+            //реализовать
+            storage[position] = r;
+        }
     }
 
     public void save(Resume r) {
-        //TODO проверка что не существует
-        //TODO проверку на переполнениe: System.out.println("...")
-        storage[lastElement] = r;
-        lastElement++;
+        // проверка что не существует
+        if (isExists(r.getUuid()) >= 0) {
+            System.out.println("Resume is already exists in storage!");
+        } else {
+            //проверку на переполнениe: System.out.println("...")
+            if (lastElement == 9999) {
+                System.out.println("Resume storage is full!");
+            } else {
+                storage[lastElement] = r;
+                lastElement++;
+            }
+        }
     }
 
     public Resume get(String uuid) {
-        //TODO проверка что существует
-        for (int i = 0; i < lastElement; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return storage[i];
+        //проверка что существует
+        if (isExists(uuid) < 0) {
+            System.out.println("Resume is not exists in storage!");
+        } else {
+            for (int i = 0; i < lastElement; i++) {
+                if (storage[i].getUuid().equals(uuid)) {
+                    return storage[i];
+                }
             }
         }
         return null;
     }
 
     public void delete(String uuid) {
-        //TODO проверка что существует
-        for (int i = 0; i < lastElement; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                storage[i] = storage[lastElement - 1];
-                storage[lastElement - 1] = null;
-                lastElement--;
-                return;
+        // проверка что существует
+        if (isExists(uuid) < 0) {
+            System.out.println("Resume is not exists in storage!");
+        } else {
+            for (int i = 0; i < lastElement; i++) {
+                if (storage[i].getUuid().equals(uuid)) {
+                    storage[i] = storage[lastElement - 1];
+                    storage[lastElement - 1] = null;
+                    lastElement--;
+                    return;
+                }
             }
         }
     }
@@ -59,5 +81,14 @@ public class ArrayStorage {
 
     public int size() {
         return lastElement;
+    }
+
+    private int isExists(String uuid) {
+        for (int i = 0; i < lastElement; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
