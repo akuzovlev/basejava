ca9ad2d5a1b20cdc70b538c50c4f488e39ca86a2
Alexander Kuzovlev <basistadom@gmail.com>
2017-10-23 12:27:57 +0300

    Lesson07_HW06 test refactoring

--- test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
+++ test/ru/javawebinar/basejava/storage/MapUuidStorageTest.java
@@ -1,23 +1,8 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import ru.javawebinar.basejava.exception.StorageException;
-
-/**
- * Created by Storm on 06.10.2017.
- */
 public class MapUuidStorageTest extends AbstractStorageTest {
 
     public MapUuidStorageTest() {
         super(new MapUuidStorage());
     }
-
-    @Ignore
-    @Test
-    @Override
-    public void saveOverflow() throws Exception {
-
-    }
-
 }
