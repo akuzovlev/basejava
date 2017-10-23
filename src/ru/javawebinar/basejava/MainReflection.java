052a755a9f9ace7b394adbe15b264ac2147c8c4c
Alexander Kuzovlev <basistadom@gmail.com>
2017-10-23 12:20:15 +0300

    Lesson07_HW06 add fullName

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -6,11 +6,10 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-
 public class MainReflection {
 
     public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        Resume r = new Resume("test");
+        Resume r = new Resume("Name");
         Class<? extends Resume> resumeClass = r.getClass();
         Field field = resumeClass.getDeclaredFields()[0];
         field.setAccessible(true);
