3b32aef7b586e921c9fd08bee61aa0f2524f33d2
Alexander Kuzovlev <basistadom@gmail.com>
2017-11-21 16:37:15 +0300

    Lesson13 Init DB

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,25 +1,13 @@
-CREATE TABLE resume
-(
-  uuid      CHAR(36) NOT NULL
-    CONSTRAINT resume_pkey
-    PRIMARY KEY,
-  full_name TEXT     NOT NULL
+CREATE TABLE resume (
+  uuid      CHAR(36) PRIMARY KEY NOT NULL,
+  full_name TEXT                 NOT NULL
 );
 
-
-CREATE TABLE contact
-(
-  id          SERIAL NOT NULL
-    CONSTRAINT contact_pkey
-    PRIMARY KEY,
-  type        TEXT   NOT NULL,
-  value       TEXT   NOT NULL,
-  resume_uuid CHAR(36)
-    CONSTRAINT contact_resume_uuid_fk
-    REFERENCES resume
-    ON DELETE CASCADE
+CREATE TABLE contact (
+  id          SERIAL,
+  resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
 );
-
 CREATE UNIQUE INDEX contact_uuid_type_index
   ON contact (resume_uuid, type);
-
